3e25b03ce7be1b9dad85e87d4ec9eb8e6cb93c29
Satish <ssatishbalathe@gmail.com>
2024-06-11 15:04:12 +0530

    Made some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes , Route} from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Container, FormWrapper, Title, Input, Button, Link } from './FormStyles';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
